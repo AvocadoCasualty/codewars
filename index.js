8493e66852b45c1e54c3c06e82882b1a606bd69a
AvocadoCasualty <baibeebambi@gmail.com>
2020-09-02 22:24:09 -0700

    More problems!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// #1
 // function solution(str, ending){
 //     if(str.endsWith(ending)){
 //         return true
@@ -9,13 +10,35 @@
 // console.log(solution('abc', 'bc'))
 // console.log(solution('abc', 'd'))
 
-function boolToWord( bool ){
-    if(bool){
-        return "Yes"
-    } else {
-        return "No"
-    }
-}
+// #2
+// function boolToWord( bool ){
+//     if(bool){
+//         return "Yes"
+//     } else {
+//         return "No"
+//     }
+// }
+//
+// console.log(boolToWord(true))
+// console.log(boolToWord(false))
 
-console.log(boolToWord(true))
-console.log(boolToWord(false))
+// #3
+// function isDivideBy(number, a, b) {
+//     if (number%a ===0 && number%b ===0){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+//
+// console.log(isDivideBy(-12,2, -6))
+// console.log(isDivideBy(-12, 2, -5))
+
+// #4
+// function getAverage(marks){
+//     sum = marks.reduce((a,b)=> a+b,0)
+//     return Math.floor(sum/marks.length)
+// }
+//
+// console.log(getAverage([2,2,2,2]))
+// console.log(getAverage([1,2,3,4,5]))
